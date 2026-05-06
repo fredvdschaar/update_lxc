176add2cf0bb9cb7aac820d1276603e89b8bfc31
Fred van der Schaar <62171569+fredvdschaar@users.noreply.github.com>
2026-05-06 13:40:14 +0200

    Create update.sh
    initial creation

---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+clear
+if test -f ~/update.dat; then
+  echo Last run:
+  cat ~/update.dat
+fi
+echo $(date) > ~/update.dat
+
+
+# Brew
+brew outdated&&brew upgrade&&brew cleanup
+
+# proxmox
+clear
+ssh root@proxmox.local 'bash -c "$(curl -fsSL https://raw.githubusercontent.com/fredvdschaar/update_lxc/refs/heads/main/update_lxc.sh)"'
+
+#pve02
+clear
+ssh root@pve02.local 'bash -c "$(curl -fsSL https://raw.githubusercontent.com/fredvdschaar/update_lxc/refs/heads/main/update_lxc.sh)"'
+
+# docker host
+#ssh fred@pi-stijn "./update.sh"
+fred@Luke-Skywalker ~ %
