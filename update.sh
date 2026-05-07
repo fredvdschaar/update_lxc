0d55bedff424bc9f2e3ff65899cc62bd6b1f221f
Fred van der Schaar <62171569+fredvdschaar@users.noreply.github.com>
2026-05-07 14:57:40 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,12 +1,41 @@
 #!/bin/bash
 clear
 
+# set colors
+set -eEuo pipefail
+YW=$(echo "\033[33m")
+BL=$(echo "\033[36m")
+RD=$(echo "\033[01;31m")
+CM='\xE2\x9C\x94\033'
+GN=$(echo "\033[1;92m")
+CL=$(echo "\033[m")
+
+# set environment vars
+SKIP_STOPPED="yes"
+NODE=$(hostname)
+excluded_containers=
+containers_needing_reboot=()
+
+### Functions
+function info_line() {
+  echo -e "${BL}[info]${GN} $1 ${CL}\n"
+}
+
+function header_info() {
+  echo -e "${GN}"
+  cat <<"EOF"
  _   _           _       _         ____  ____  _______        __
 | | | |_ __   __| | __ _| |_ ___  | __ )|  _ \| ____\ \      / /
 | | | | '_ \ / _` |/ _` | __/ _ \ |  _ \| |_) |  _|  \ \ /\ / /
 | |_| | |_) | (_| | (_| | ||  __/ | |_) |  _ <| |___  \ V  V /
  \___/| .__/ \__,_|\__,_|\__\___| |____/|_| \_\_____|  \_/\_/
       |_|
+
+EOF
+  info_line "Working on server [$(hostname -f)]"
+}
+
+header_info
 
       
 # show the last time this script ran
@@ -29,7 +58,7 @@ clear
 for hostName in proxmox.local pve02.local ;
   do
     clear
-    ssh $user@$hostName 'bash -c "$(curl -fsSL https://raw.githubusercontent.com/$gituser/update_lxc/refs/heads/main/update_lxc.sh)"'
+    bash ssh $user@$hostName 'bash -c "$(curl -fsSL https://raw.githubusercontent.com/$gituser/update_lxc/refs/heads/main/update_lxc.sh)"'
 
   done
 
